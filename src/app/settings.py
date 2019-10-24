9adf4cd807cae9750d267e2bbd5b2b700917261e
Michael Feldstein <msfeldstein@gmail.com>
2019-10-24 10:42:58 -0700

    Update Interactive UI (#147)
    * add a base css
    
    * Update reciept page
    
    * Try to fix tests

--- src/app/settings.py
+++ src/app/settings.py
@@ -142,8 +142,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Django Rest Framework Settings:
 
 REST_FRAMEWORK = {
