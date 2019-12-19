8256400f6520f9bcb3c54aa56ba761c5fa34f5cc
Jake Urban <jake@stellar.org>
2019-12-19 11:53:27 -0800

    Added readonly user credentials to login page

--- example/server/urls.py
+++ example/server/urls.py
@@ -13,7 +13,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth import views
+from django.urls import path, re_path, include
 import polaris.urls
 
-urlpatterns = [path("admin", admin.site.urls), path("", include(polaris.urls))]
+urlpatterns = [
+    re_path(
+        "login",
+        views.LoginView.as_view(template_name="readonly_login.html"),
+        name="login",
+    ),
+    path("admin", admin.site.urls),
+    path("", include(polaris.urls)),
+]
