6f9a4e42736d24f283306f23ca0ab77eaa2f7fa9
Jake Urban <jake@stellar.org>
2019-12-20 10:35:57 -0800

    Fixed path to example settings module

--- example/server/settings.py
+++ example/server/settings.py
@@ -15,7 +15,7 @@ import environ
 from shutil import copyfile
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = BASE_DIR
 
 # Load environment variables from .env
