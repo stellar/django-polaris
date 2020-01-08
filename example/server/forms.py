6dd7d398506f8c90add1b445f30ebe6569c95dab
Jake Urban <jake@stellar.org>
2020-01-07 16:19:04 -0800

    Added 'input' class for KYCForm fields

--- example/server/forms.py
+++ example/server/forms.py
@@ -2,6 +2,10 @@ from django import forms
 
 
 class KYCForm(forms.Form):
-    first_name = forms.CharField(max_length=254)
-    last_name = forms.CharField(max_length=254)
-    email = forms.EmailField()
+    first_name = forms.CharField(
+        max_length=254, widget=forms.TextInput(attrs={"class": "input"})
+    )
+    last_name = forms.CharField(
+        max_length=254, widget=forms.TextInput(attrs={"class": "input"})
+    )
+    email = forms.EmailField(widget=forms.TextInput(attrs={"class": "input"}))
