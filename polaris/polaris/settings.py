0dbb35bccbd8121a35ac9176cb4f45bd556e24fc
Jake Urban <jake@stellar.org>
2020-01-13 10:38:48 -0800

    moving back to .env instead of config.yml

--- polaris/polaris/settings.py
+++ polaris/polaris/settings.py
@@ -3,45 +3,52 @@ Polaris-specific settings. This is not django.conf.settings.
 """
 # pylint: disable=invalid-name
 import os
-import yaml
+import environ
 from django.conf import settings
 from stellar_sdk.server import Server
 from stellar_sdk.keypair import Keypair
+from stellar_sdk.exceptions import Ed25519SecretSeedInvalidError
 
 
-config_filepath = os.path.join(settings.PROJECT_ROOT, "config.yml")
+env = environ.Env()
+env_file = os.path.join(settings.PROJECT_ROOT, ".env")
+if os.path.exists(env_file):
+    environ.Env.read_env(str(env_file))
+
 try:
-    config = yaml.safe_load(open(config_filepath).read())
-except FileNotFoundError:
-    raise ValueError("Missing config.yml file.")
-except yaml.YAMLError as e:
-    raise ValueError(f"Error parsing yaml file: {str(e)}")
+    assets = env.list("ASSETS")
+    assert len(assets)
+except (environ.ImproperlyConfigured, AssertionError):
+    raise ValueError("ASSETS must be set in your .env file")
 
-if not config.get("assets"):
-    raise ValueError("You must provide at least one asset to anchor")
-for asset_type, values in config["assets"].items():
-    if not (
-        values.get("distribution_account_seed") and values.get("issuer_account_address")
-    ):
-        raise ValueError(f"Missing values for {asset_type}")
+ASSETS = {}
+for asset_code in assets:
+    code = asset_code.upper()
 
-# Stellar Settings
-ASSETS = {
-    asset_type.upper(): {
-        "DISTRIBUTION_ACCOUNT_SEED": values.get("distribution_account_seed"),
-        "DISTRIBUTION_ACCOUNT_ADDRESS": Keypair.from_secret(
-            values.get("distribution_account_seed")
-        ).public_key,
-        "ISSUER_ACCOUNT_ADDRESS": values.get("issuer_account_address"),
+    try:
+        dist_seed = env(f"{code}_DISTRIBUTION_ACCOUNT_SEED")
+        iss_address = env(f"{code}_ISSUER_ACCOUNT_ADDRESS")
+        assert dist_seed and iss_address
+    except (environ.ImproperlyConfigured, AssertionError):
+        raise ValueError(f"Missing values for {code}")
+
+    try:
+        dist_address = Keypair.from_secret(dist_seed).public_key
+    except Ed25519SecretSeedInvalidError:
+        raise ValueError(f"Invalid distribution private key for {code}")
+
+    ASSETS[code] = {
+        "DISTRIBUTION_ACCOUNT_SEED": dist_seed,
+        "DISTRIBUTION_ACCOUNT_ADDRESS": dist_address,
+        "ISSUER_ACCOUNT_ADDRESS": iss_address,
     }
-    for asset_type, values in config.get("assets", {}).items()
-}
-STELLAR_NETWORK_PASSPHRASE = config.get(
-    "stellar_network_passphrase", "Test SDF Network ; September 2015"
+
+STELLAR_NETWORK_PASSPHRASE = env(
+    "STELLAR_NETWORK_PASSPHRASE", default="Test SDF Network ; September 2015"
 )
-HORIZON_URI = config.get("horizon_uri", "https://horizon-testnet.stellar.org/")
+HORIZON_URI = env("HORIZON_URI", default="https://horizon-testnet.stellar.org/")
 HORIZON_SERVER = Server(horizon_url=HORIZON_URI)
-SERVER_JWT_KEY = config.get("secret_jwt_key")
+SERVER_JWT_KEY = env("SERVER_JWT_KEY")
 OPERATION_DEPOSIT = "deposit"
 OPERATION_WITHDRAWAL = "withdraw"
 ACCOUNT_STARTING_BALANCE = str(2.01)
