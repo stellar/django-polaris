a0108f7d41b071d3ad592dfaf9f9895685002493
Jake Urban <jake@stellar.org>
2020-01-14 12:38:30 -0800

    Added compilescss command to run.sh

--- polaris/run.sh
+++ polaris/run.sh
@@ -1,3 +1,4 @@
 pipenv run python manage.py migrate
+pipenv run python manage compilescss
 pipenv run python manage.py collectstatic --no-input -v 0
 pipenv run python manage.py runsslserver --nostatic --certificate cert.pem --key key.pem 0.0.0.0:8000
