0adff32370c219dbee4de0a82ff2cc97a1547ff1
Jake Urban <jake@stellar.org>
2020-01-17 14:04:39 -0800

    Updated example settings

--- example/server/settings.py
+++ example/server/settings.py
@@ -113,7 +113,8 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-LANGUAGES = [("en", _("English")), ("es", _("Spanish")), ("pt", _("Portuguese"))]
+USE_THOUSAND_SEPARATOR = True
+LANGUAGES = [("en", _("English")), ("pt", _("Portuguese"))]
 
 
 # Static files (CSS, JavaScript, Images)
