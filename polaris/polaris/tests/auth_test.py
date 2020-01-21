d4512296d141105ad4da120b4b953f7a900c5d7f
Jake Urban <jake@stellar.org>
2020-01-21 11:29:07 -0800

    Adjust other uses of signing key

--- polaris/polaris/tests/auth_test.py
+++ polaris/polaris/tests/auth_test.py
@@ -44,11 +44,8 @@ def test_auth_get_account(client):
     assert len(signatures) == 1
     server_signature = signatures[0]
 
-    default_asset = list(settings.ASSETS)[0]
     tx_hash = envelope_object.hash()
-    server_public_key = Keypair.from_public_key(
-        settings.ASSETS[default_asset]["DISTRIBUTION_ACCOUNT_ADDRESS"]
-    )
+    server_public_key = Keypair.from_public_key(settings.SIGNING_KEY)
     server_public_key.verify(tx_hash, server_signature.signature)
 
 
