2bc9c4ee06b485ff93a6ff084e3e5bb8253bc84a
Jake Urban <jake@stellar.org>
2020-01-27 11:19:24 -0800

    Use stellar-sdk 2.1.1-beta1 for SEP-10 multisig support

--- polaris/polaris/sep10auth/views.py
+++ polaris/polaris/sep10auth/views.py
@@ -16,13 +16,13 @@ from rest_framework.decorators import api_view
 from stellar_sdk.transaction_envelope import TransactionEnvelope
 from stellar_sdk.sep.stellar_web_authentication import (
     build_challenge_transaction,
-    verify_challenge_transaction,
+    read_challenge_transaction,
+    verify_challenge_transaction_threshold,
+    verify_challenge_transaction_signed_by_client,
 )
 from stellar_sdk.sep.exceptions import InvalidSep10ChallengeError
-from stellar_sdk.keypair import Keypair
 from stellar_sdk.exceptions import (
     Ed25519PublicKeyInvalidError,
-    BadSignatureError,
     NotFoundError,
 )
 
@@ -96,137 +96,54 @@ def _transaction_from_post_request(request):
 
 def _validate_challenge_xdr(envelope_xdr):
     """
-    Validate the provided TransactionEnvelope XDR (base64 string). Return the
-    appropriate error if it fails, else the empty string.
+    Validate the provided TransactionEnvelope XDR (base64 string).
 
-    verify_challenge_transaction() does not verify that the transaction's
-    signers are valid and have the total weight greater or equal to the
-    source account's medium threshold value, so this function does that as
-    well.
+    If the source account of the challenge transaction exists, verify the weight
+    of the signers on the challenge are signers for the account and the medium
+    threshold on the account is met by those signers.
+
+    If the source account does not exist, verify that the keypair used as the
+    source for the challenge transaction has signed the challenge. This is
+    sufficient because newly created accounts have their own keypair as signer
+    with a weight greater than the default thresholds.
     """
+    server_key = settings.SIGNING_KEY
+    net = settings.STELLAR_NETWORK_PASSPHRASE
+
+    logger.info("Validating challenge transaction")
     try:
-        # For now, this function assumes the client's account has signed
-        # the transaction envelope, so it doesn't yet support multisig
-        # challenges where the client account isn't a signer. We'll be
-        # making a pull request to add multisig support.
-        verify_challenge_transaction(
-            challenge_transaction=envelope_xdr,
-            server_account_id=settings.SIGNING_KEY,
-            network_passphrase=settings.STELLAR_NETWORK_PASSPHRASE,
+        tx_envelope, account_id = read_challenge_transaction(
+            envelope_xdr, server_key, net
         )
     except InvalidSep10ChallengeError as e:
+        err_msg = f"Error while validating challenge: {str(e)}"
+        logger.error(err_msg)
+        raise ValueError(err_msg)
+
+    try:
+        account = settings.HORIZON_SERVER.load_account(account_id)
+    except NotFoundError:
+        logger.warning("Account does not exist, using client's master key to verify")
+        try:
+            verify_challenge_transaction_signed_by_client(envelope_xdr, server_key, net)
+        except InvalidSep10ChallengeError as e:
+            logger.info(f"Missing or invalid signature(s) for {account_id}: {str(e)})")
+            raise ValueError(str(e))
+        else:
+            logger.info("Challenge verified using client's master key")
+            return
+
+    signers = account.load_ed25519_public_key_signers()
+    threshold = account.thresholds.med_threshold
+    try:
+        signers_found = verify_challenge_transaction_threshold(
+            envelope_xdr, server_key, net, threshold, signers
+        )
+    except InvalidSep10ChallengeError as e:
+        logger.info(str(e))
         raise ValueError(str(e))
 
-    transaction_envelope = TransactionEnvelope.from_xdr(
-        envelope_xdr, settings.STELLAR_NETWORK_PASSPHRASE
-    )
-    # verify_challenge_transaction ensures transaction has at least
-    # one operation whose source is the client account.
-    source_account = transaction_envelope.transaction.operations[0].source
-    tx_hash = transaction_envelope.hash()
-
-    # make horizon /accounts API call to get signers and threshold
-    try:
-        account_signers, threshold = _get_signers_and_threshold(source_account)
-    except NotFoundError:
-        # The account doesn't exist, so we cannot check the signers.
-        #
-        # This is fine because a newly created account's single signer
-        # is itself. When the necessary changes go into stellar_sdk,
-        # verify_challenge_transaction() will check that the client
-        # has signed the envelope XDR when account_signers isn't passed
-        return
-
-    ##################################################################
-    # The functionality implemented below should go in stellar_sdk and
-    # invoked like so:
-    #
-    #       verify_challenge_transaction(
-    #           challenge_transaction=envelope_xdr,
-    #           server_account_id=settings.SIGNING_KEY,
-    #           network_passphrase=settings.STELLAR_NETWORK_PASSPHRASE,
-    #           account_signers=account_signers,
-    #           threshold=threshold
-    #       )
-    #
-    # Where account_signers and threshold are defined like so:
-    #
-    #       account_json = server.accounts().account_id(source_account).call()
-    #       account_signers = account_json["signers"]
-    #       threshold = account_json["thresholds"]["med_threshold"]
-    #
-    # This way, the client can choose which threshold the challenge should
-    # meet. It also makes sense to pass the unaltered account_json["signers"]
-    # instead of requiring the client to do some manipulation of the
-    # response before passing.
-    #
-    # The stellar_sdk should also have a function to extract the transaction
-    # and source_account so the client doesn't have to know to fetch them
-    # from envelope_xdr
-    ##################################################################
-
-    # Get the account signers used for this transaction
-    server_kp = Keypair.from_public_key(settings.SIGNING_KEY)
-    matched_signers = {}  # use signer pub key to avoid duplicate signatures
-    for dec_signer in transaction_envelope.signatures:
-        if dec_signer.hint != server_kp.signature_hint():
-            ms = _match_signer(tx_hash, dec_signer, account_signers)
-            matched_signers[ms["keypair"].public_key] = ms
-            continue
-        try:
-            # dec_signer might be derived from server_kp
-            server_kp.verify(tx_hash, dec_signer.signature)
-        except BadSignatureError:
-            # dec_signer is not derived from server_kp
-            ms = _match_signer(tx_hash, dec_signer, account_signers)
-            matched_signers[ms["keypair"].public_key] = ms
-
-    # Check threshold
-    if sum(signer["weight"] for signer in matched_signers.values()) < threshold:
-        raise ValueError(
-            "Transaction signers do not reach medium threshold for account"
-        )
-
-
-def _match_signer(tx_hash, dec_signer, account_signers):
-    """
-    Iterate over account_signers to find a match for dec_signer. If
-    dec_signer doesn't have a match, raise a ValueError.
-    """
-    matched_signer = None
-    for acc_signer in account_signers:
-        account_kp = acc_signer["keypair"]
-        if dec_signer.hint != account_kp.signature_hint():
-            continue
-        try:
-            account_kp.verify(tx_hash, dec_signer.signature)
-        except BadSignatureError:
-            continue
-        else:
-            matched_signer = acc_signer
-            break
-
-    if not matched_signer:
-        raise ValueError("Transaction has unrecognized signatures")
-
-    return matched_signer
-
-
-def _get_signers_and_threshold(source_account):
-    """
-    Makes a Horizon API call to /accounts and returns the signers and
-    threshold. Replaces 'key' with 'keypair'.
-
-    May raise a NotFoundError if the account does not exist.
-    """
-    server = settings.HORIZON_SERVER
-    account_json = server.accounts().account_id(source_account).call()
-    threshold = account_json["thresholds"]["med_threshold"]
-    account_signers = []
-    for signer in account_json["signers"]:
-        kp = Keypair.from_public_key(signer.pop("key"))
-        account_signers.append({"keypair": kp, **signer})
-    return account_signers, threshold
+    logger.info(f"Challenge verified using account signers: {signers_found}")
 
 
 def _generate_jwt(request, envelope_xdr):
