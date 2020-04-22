aef3d3eeaf29588496033f8a4985bc5622309aaa
Jake Urban <jake@stellar.org>
2020-04-22 15:39:06 -0700

    add deposit endpoint to urls.py

--- polaris/polaris/sep6/urls.py
+++ polaris/polaris/sep6/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from polaris.sep6 import info
+from polaris.sep6 import info, deposit
 from polaris.sep12 import customer
 
 urlpatterns = [
-    # path("deposit", ),
+    path("deposit", deposit.deposit),
     # path("withdraw", ),
     path("info", info.info),
     # path("fee", ),
     # path("transaction", ),
     # path("transactions", ),
-    path("customer", customer.put_customer),
-    path("customer/<account>", customer.delete_customer),
 ]
