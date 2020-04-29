f39a42ea4a3b2e87ae2d6d4ece93efddf3bf7d41
Jake Urban <jake@stellar.org>
2020-04-29 12:43:58 -0700

    Only require SIGNING_KEY if SEP-10 is enabled

--- polaris/polaris/settings.py
+++ polaris/polaris/settings.py
@@ -8,16 +8,18 @@ from django.conf import settings
 from stellar_sdk.server import Server
 from stellar_sdk.keypair import Keypair
 
-from polaris.models import Asset
-
 
 env = environ.Env()
 env_file = os.path.join(settings.PROJECT_ROOT, ".env")
 if os.path.exists(env_file):
     environ.Env.read_env(str(env_file))
 
-SIGNING_SEED = env("SIGNING_SEED")
-SIGNING_KEY = Keypair.from_secret(SIGNING_SEED).public_key
+# The SIGNING_KEY should probably be independent of the assets being anchored,
+# but if they are not specified we can use the first distribution account.
+SIGNING_SEED, SIGNING_KEY = None, None
+if "sep-10" in settings.ACTIVE_SEPS:
+    SIGNING_SEED = env("SIGNING_SEED")
+    SIGNING_KEY = Keypair.from_secret(SIGNING_SEED).public_key
 
 STELLAR_NETWORK_PASSPHRASE = env(
     "STELLAR_NETWORK_PASSPHRASE", default="Test SDF Network ; September 2015"
