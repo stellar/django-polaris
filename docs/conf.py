b865d3539235921898cedb0e4026eb4206885cf6
Jake Urban <jake@stellar.org>
2020-07-08 14:19:54 -0700

    prep for 0.11.2 release

--- docs/conf.py
+++ docs/conf.py
@@ -33,9 +33,9 @@ copyright = "2019, Jake Urban"
 author = "Jake Urban"
 
 # The short X.Y version
-version = "0.11.1"
+version = "0.11.2"
 # The full version, including alpha/beta/rc tags
-release = "0.11.1"
+release = "0.11.2"
 
 
 # -- General configuration ---------------------------------------------------
