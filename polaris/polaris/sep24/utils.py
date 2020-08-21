feb62a4edbca0b141bce567a413901c00bd1ca78
Jake Urban <jake@stellar.org>
2020-08-20 18:09:37 -0700

    ensure log statements are given strings

--- polaris/polaris/sep24/utils.py
+++ polaris/polaris/sep24/utils.py
@@ -38,7 +38,7 @@ def check_authentication(content_type: str = "text/html") -> Callable:
 
             response = view(request)
             logger.info("Got response, checking for session cookie")
-            logger.info(repr(response.cookies), type(response.cookies))
+            logger.info(repr(response.cookies), str(type(response.cookies)))
             logger.info(request.session.items())
             if (
                 django_settings.SESSION_COOKIE_NAME in response.cookies
