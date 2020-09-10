ce388e2e79853e9c3467577c5c379047797b6c11
Jake Urban <jake@stellar.org>
2020-09-10 14:20:53 -0700

    update version for 1.0 release

--- docs/conf.py
+++ docs/conf.py
@@ -33,9 +33,9 @@ copyright = "2019, Jake Urban"
 author = "Jake Urban"
 
 # The short X.Y version
-version = "0.13.0"
+version = "1.0.0"
 # The full version, including alpha/beta/rc tags
-release = "0.13.0"
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
