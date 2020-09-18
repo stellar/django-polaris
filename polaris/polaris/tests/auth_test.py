9f170390b9690d3c999145a0e2e2fee3f5e22414
Jake Urban <jake@stellar.org>
2020-09-18 11:31:57 -0700

    add test fix

--- polaris/polaris/tests/auth_test.py
+++ polaris/polaris/tests/auth_test.py
@@ -42,7 +42,7 @@ def test_auth_get_account(client):
 
     manage_data_op = transaction_object.operations[0]
     assert manage_data_op.type_code() == Xdr.const.MANAGE_DATA
-    assert manage_data_op.data_name == "SEP 24 Reference auth"
+    assert manage_data_op.data_name == "localhost:8000 auth"
     assert len(manage_data_op.data_value) == 64
     assert len(base64.b64decode(manage_data_op.data_value)) == 48
 
