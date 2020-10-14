6ce7f56ad089a3fcf3f5bb83d9c4502bae70d5da
Jake Urban <jake@stellar.org>
2020-10-14 12:31:17 -0700

    updated requirements, bumped version

--- docs/conf.py
+++ docs/conf.py
@@ -33,9 +33,9 @@ copyright = "2019, Jake Urban"
 author = "Jake Urban"
 
 # The short X.Y version
-version = "1.1.1"
+version = "1.1.2"
 # The full version, including alpha/beta/rc tags
-release = "1.1.1"
+release = "1.1.2"
 
 
 # -- General configuration ---------------------------------------------------
