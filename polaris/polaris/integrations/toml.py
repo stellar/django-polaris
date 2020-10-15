126558e3e630480c481bcd1af2b552be292d7070
Jake Urban <10968980+JakeUrban@users.noreply.github.com>
2020-10-15 08:45:10 -0700

    Static Stellar TOMLs (#337)

--- polaris/polaris/integrations/toml.py
+++ polaris/polaris/integrations/toml.py
@@ -1,4 +1,3 @@
-from polaris import settings
 from polaris.models import Asset
 
 
@@ -7,21 +6,26 @@ def get_stellar_toml():
     .. _SEP-1: https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0001.md
     .. _`Account Info`: https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0001.md#account-information
 
-    Replace this function with another by passing it to
-    ``register_integrations()`` as described in
-    :doc:`Registering Integrations</register_integrations/index>`.
+    Replace this function with another by passing it to ``register_integrations()``
+    as described in :doc:`Registering Integrations</register_integrations/index>`.
 
-    The function you pass to the `toml` parameter should return a
-    dictionary containing any of the following top level keys:
-    ``DOCUMENTATION`` - ``CURRENCIES`` - ``PRINCIPALS`` - ``VALIDATORS``
+    The dictionary returned will be merged with Polaris' default attributes and serialized
+    using the ``toml.dumps()`` function. The output will be rendered in the HTTP response.
 
-    You can also pass other top-level keys defined in the `Account Info`_
-    section such as ``VERSION``, but many of these keys are pre-populated based on
-    the variables defined in your `.env` file, so this isn't strictly necessary.
+    The base attributes provided by Polaris are:
 
-    See SEP-1_ for more information on the stellar.toml format.
+    - `ACCOUNTS`
+    - `VERSION`
+    - `SIGNING_KEY`
+    - `NETWORK_PASSPHRASE`
+    - `TRANSFER_SERVER`
+    - `TRANSFER_SERVER_0024`
+    - `KYC_SERVER`
+    - `DIRECT_PAYMENT_SERVER`
 
-    :return: a dictionary containing the fields defined in SEP-1_
+    The contents of the dictionary returned will overwrite the default matching key values.
+
+    :return: a dictionary of SEP-1_ attributes
     """
     return {
         "CURRENCIES": [
--- polaris/polaris/sep1/views.py
+++ polaris/polaris/sep1/views.py
@@ -7,18 +7,60 @@ https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0001.md
 """
 
 import os
+import sys
 
 import toml
-from django.http.response import HttpResponse
 from django.conf import settings as django_settings
+from django.contrib.staticfiles import finders
+from django.utils.encoding import smart_text
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.renderers import BaseRenderer
+from rest_framework.decorators import api_view, renderer_classes
 
 from polaris import settings
-from polaris.integrations import registered_toml_func
+from polaris.utils import getLogger
+from polaris.integrations import registered_toml_func, get_stellar_toml
 from polaris.models import Asset
 
 
-def generate_toml(request):
-    """Generate the TOML file."""
+logger = getLogger(__name__)
+
+
+class PolarisPlainTextRenderer(BaseRenderer):
+    """
+    .. _documentation: https://www.django-rest-framework.org/api-guide/renderers/#custom-renderers
+
+    Source copied from django-rest-framework's documentation_
+    """
+
+    media_type = "text/plain"
+    format = "txt"
+
+    def render(self, data, media_type=None, renderer_context=None):
+        return smart_text(data, encoding=self.charset)
+
+
+@api_view(["GET"])
+@renderer_classes([PolarisPlainTextRenderer])
+def generate_toml(request: Request) -> Response:
+    """Generate a TOML-formatted string"""
+    # Define the module variable to reference in-memory constants
+    # Check if we've already read the TOML contents
+    this = sys.modules[__name__]
+    if hasattr(this, "STELLAR_TOML_CONTENTS"):
+        return Response(this.STELLAR_TOML_CONTENTS, content_type="text/plain")
+
+    # Check for a static TOML file if the cache is empty and the TOML
+    # integration function is not replaced.
+    if registered_toml_func is get_stellar_toml:
+        static_toml = finders.find("polaris/stellar.toml")
+        if static_toml:
+            with open(static_toml) as f:
+                this.STELLAR_TOML_CONTENTS = f.read()
+            return Response(this.STELLAR_TOML_CONTENTS, content_type="text/plain")
+
+    # The anchor uses the registered TOML function, replaced or not
     toml_dict = {
         "ACCOUNTS": [
             asset.distribution_account
@@ -28,7 +70,6 @@ def generate_toml(request):
         "SIGNING_KEY": settings.SIGNING_KEY,
         "NETWORK_PASSPHRASE": settings.STELLAR_NETWORK_PASSPHRASE,
     }
-
     if "sep-24" in django_settings.POLARIS_ACTIVE_SEPS:
         toml_dict["TRANSFER_SERVER"] = os.path.join(settings.HOST_URL, "sep24")
         toml_dict["TRANSFER_SERVER_SEP0024"] = toml_dict["TRANSFER_SERVER"]
@@ -42,5 +83,9 @@ def generate_toml(request):
         toml_dict["DIRECT_PAYMENT_SERVER"] = os.path.join(settings.HOST_URL, "sep31")
 
     toml_dict.update(registered_toml_func())
+    # We could assign content to this.STELLAR_TOML_CONTENTS, but if the anchor hasn't
+    # transitioned to the static file approach, it's possible the anchor does not want
+    # the TOML contents to be cached.
+    content = toml.dumps(toml_dict)
 
-    return HttpResponse(toml.dumps(toml_dict), content_type="text/plain")
+    return Response(content, content_type="text/plain")
