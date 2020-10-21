0d9972192568b9af68940a215d20fdef09c55e2b
Lawrence <lawrence@stellar.org>
2020-10-21 11:01:18 -0700

    parse and check for all results.
    - there is more than one operation we want to ensure we dont fail if
    createClaimableBalanceResult is the 1st..nth tr in the xdr
    - This will return too early in the case where there are multiple
    createClaimableBalanceResult operations on a singe transaction
      - TODO: change the model attr to be an array of strings?
    
    Signed-off-by: Lawrence <lawrence@stellar.org>

--- polaris/polaris/utils.py
+++ polaris/polaris/utils.py
@@ -448,9 +448,11 @@ def create_transaction_envelope(transaction, source_account) -> TransactionEnvel
 def get_balanceid(response):
     claimable_balance_res = response['result_xdr']
     tr_xdr = TransactionResult.from_xdr(claimable_balance_res)
-    cbr_xdr = base64.b64decode(
-        tr_xdr.result.results[0].tr.createClaimableBalanceResult.balanceID.to_xdr())
-    return cbr_xdr.hex()
+    for tr in tr_xdr.result.results:
+        if hasattr(tr.tr, 'createClaimableBalanceResult'):
+            cbr_xdr = base64.b64decode(
+                tr.tr.createClaimableBalanceResult.balanceID.to_xdr())
+            return cbr_xdr.hex()
 
 
 def memo_str(memo: str, memo_type: str) -> Optional[str]:
