f6fefb630ca6276ae454bd7a9a0cdc86a9c3d931
Lawrence Lawson <lawrence@stellar.org>
2020-12-15 10:01:51 -0800

    remove unneeded test
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- polaris/polaris/tests/processes/test_create_stellar_deposit.py
+++ polaris/polaris/tests/processes/test_create_stellar_deposit.py
@@ -120,51 +120,6 @@ def test_deposit_stellar_success(acc1_usd_deposit_transaction_factory):
     assert Transaction.objects.get(id=deposit.id).status == Transaction.STATUS.completed
 
 
-no_trust_exp = BaseHorizonError(
-    Mock(
-        json=Mock(
-            return_value={
-                "extras": {"result_xdr": "AAAAAAAAAGT/////AAAAAQAAAAAAAAAB////+gAAAAA="}
-            }
-        )
-    )
-)
-
-
-@pytest.mark.django_db
-@patch(
-    "polaris.utils.settings.HORIZON_SERVER",
-    Mock(
-        load_account=Mock(return_value=mock_account),
-        submit_transaction=Mock(side_effect=no_trust_exp),
-        fetch_base_fee=Mock(return_value=100),
-    ),
-)
-@patch(
-    "polaris.utils.get_account_obj", Mock(return_value=(mock_account, {"balances": []}))
-)
-def test_deposit_stellar_no_trustline(acc1_usd_deposit_transaction_factory):
-    """
-    `create_stellar_deposit` sets the transaction with the provided `transaction_id` to
-    status `pending_trust` if the provided transaction's Stellar account has no trustline
-    for its asset. (We assume the asset's issuer is the server Stellar account.)
-
-    This is the flow when a wallet/client submits a deposit without the sending
-    "claimable_balance_supported=True" in their POST request body.
-    or sending "claimable_balance_supported=False" in their POST request body.
-    Such that the deposit transaction is blocked
-    until the Wallet user creates a trustline to that asset
-    """
-    deposit = acc1_usd_deposit_transaction_factory()
-    deposit.status = Transaction.STATUS.pending_anchor
-    deposit.save()
-    assert not create_stellar_deposit(deposit)
-    assert (
-        Transaction.objects.get(id=deposit.id).status
-        == Transaction.STATUS.pending_trust
-    )
-
-
 mock_server_no_trust_account_claim = Mock(
     accounts=Mock(
         return_value=Mock(
