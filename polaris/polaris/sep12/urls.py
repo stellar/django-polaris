7b0b6473f65596a81d97b8404271523b15d6669a
Jake Urban <jake@stellar.org>
2021-03-05 17:14:34 -0800

    reorder SEP-12 url pattern matching

--- polaris/polaris/sep12/urls.py
+++ polaris/polaris/sep12/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from polaris.sep12 import customer
 
 urlpatterns = [
-    path("customer/<account>", customer.delete),
     path("customer/callback", customer.callback),
+    path("customer/<account>", customer.delete),
     path("customer", customer.CustomerAPIView.as_view()),
 ]
