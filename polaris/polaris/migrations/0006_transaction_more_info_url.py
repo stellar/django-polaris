034e270e921d4c0f271f1fb9c5e06df1eb0ea9ae
Jake Urban <jake@stellar.org>
2021-03-19 10:04:41 -0700

    only execute more_info migration for transactions of an active SEP

--- polaris/polaris/migrations/0006_transaction_more_info_url.py
+++ polaris/polaris/migrations/0006_transaction_more_info_url.py
@@ -9,20 +9,22 @@ from polaris.shared.endpoints import SEP6_MORE_INFO_PATH
 
 def populate_more_info_urls(apps, _):
     Transaction = apps.get_model("polaris", "Transaction")
-    Transaction.objects.filter(protocol="sep24").update(
-        more_info_url=Concat(
-            models.Value(f"{settings.HOST_URL}{reverse('more_info')}?id="),
-            models.F("id"),
-            output_field=models.TextField(null=True, blank=True),
-        ),
-    )
-    Transaction.objects.filter(protocol="sep6").update(
-        more_info_url=Concat(
-            models.Value(f"{settings.HOST_URL}{SEP6_MORE_INFO_PATH}?id="),
-            models.F("id"),
-            output_field=models.TextField(null=True, blank=True),
-        ),
-    )
+    if "sep-24" in settings.ACTIVE_SEPS:
+        Transaction.objects.filter(protocol="sep24").update(
+            more_info_url=Concat(
+                models.Value(f"{settings.HOST_URL}{reverse('more_info')}?id="),
+                models.F("id"),
+                output_field=models.TextField(null=True, blank=True),
+            ),
+        )
+    if "sep-6" in settings.ACTIVE_SEPS:
+        Transaction.objects.filter(protocol="sep6").update(
+            more_info_url=Concat(
+                models.Value(f"{settings.HOST_URL}{SEP6_MORE_INFO_PATH}?id="),
+                models.F("id"),
+                output_field=models.TextField(null=True, blank=True),
+            ),
+        )
 
 
 def noop(*_):
