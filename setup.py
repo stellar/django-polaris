18af15770ef819e128132792e87765c0a1159668
Jake Urban <jake@stellar.org>
2021-08-26 15:31:06 -0700

    bump version to 1.6.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst") as f:
 
 setup(
     name="django-polaris",
-    version="1.6.0",
+    version="1.6.1",
     description="An extendable Django server for Stellar Ecosystem Proposals",
     long_description=long_description,
     long_description_content_type="text/x-rst",
