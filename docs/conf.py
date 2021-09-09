9c531112690ec71dffbb0b68d09d83b3c729f489
Jake Urban <jake@stellar.org>
2021-09-09 15:51:44 -0700

    bump version to 2.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -33,9 +33,9 @@ copyright = "2019, Jake Urban"
 author = "Jake Urban"
 
 # The short X.Y version
-version = "2.0.1"
+version = "2.0.2"
 # The full version, including alpha/beta/rc tags
-release = "2.0.1"
+release = "2.0.2"
 
 
 # -- General configuration ---------------------------------------------------
