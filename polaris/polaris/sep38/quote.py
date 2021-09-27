7a51d81744236c1966033519c35eb1169a42dfb7
Jake Urban <jake@stellar.org>
2021-09-27 16:53:29 -0700

    add stub for quote validation

--- polaris/polaris/sep38/quote.py
+++ polaris/polaris/sep38/quote.py
@@ -17,6 +17,10 @@ from polaris.sep10.utils import validate_sep10_token
 from polaris.utils import render_error_response
 from polaris.sep38.serializers import QuoteSerializer
 from polaris.sep38.utils import get_buy_asset, get_sell_asset
+from polaris.utils import getLogger
+
+
+logger = getLogger(__name__)
 
 
 @api_view(["GET"])
@@ -39,15 +43,28 @@ def post_quote(token: SEP10Token, request: Request) -> Response:
         request_data = validate_quote_request(token, request)
     except ValueError as e:
         return render_error_response(str(e))
-    quote = rqi.post_quote(**request_data)
+
+    try:
+        quote = rqi.post_quote(**request_data)
+    except ValueError as e:
+        return render_error_response(str(e), status_code=400)
+    except RuntimeError as e:
+        return render_error_response(str(e), status_code=503)
+
+    try:
+        validate_quote_provided(quote)
+    except ValueError as e:
+        logger.error(gettext("invalid quote provided: ") + str(e))
+        return render_error_response("internal server error", status_code=500)
+
     quote.save()
-    return Response(QuoteSerializer(quote))
+    return Response(QuoteSerializer(quote).data)
 
 
 def validate_quote_request(token: SEP10Token, request: Request) -> dict:
     validated_data: Dict = {"token": token, "request": request}
     required_fields = ["sell_asset", "sell_amount", "buy_asset", "buy_amount"]
-    if any(f not in required_fields for f in request.data.keys()):
+    if not set(required_fields).issubset(request.GET.keys()):
         raise ValueError(
             gettext("missing required parameters. Required: ")
             + ", ".join(required_fields)
@@ -71,6 +88,7 @@ def validate_quote_request(token: SEP10Token, request: Request) -> dict:
     validated_data["sell_asset"] = get_sell_asset(
         sell_asset_str=request.data["sell_asset"],
         sell_delivery_method=request.data.get("buy_delivery_method"),
+        country_code=request.data.get("country_code"),
     )
     validated_data["buy_asset"] = get_buy_asset(
         sell_asset=validated_data["sell_asset"],
@@ -89,9 +107,13 @@ def validate_quote_request(token: SEP10Token, request: Request) -> dict:
         )
     except DecimalException:
         raise ValueError(
-            gettext("Invalid 'buy_amount' or 'sell_amount'; Expected decimal strings.")
+            gettext("invalid 'buy_amount' or 'sell_amount'; Expected decimal strings.")
         )
     validated_data["country_code"] = request.data.get("country_code")
     validated_data["buy_delivery_method"] = request.data.get("buy_delivery_method")
     validated_data["sell_delivery_method"] = request.data.get("sell_delivery_method")
     return validated_data
+
+
+def validate_quote_provided(quote: Quote):
+    pass
--- polaris/polaris/tests/sep38/test_quote.py
+++ polaris/polaris/tests/sep38/test_quote.py
@@ -1,234 +1,50 @@
-from unittest import mock
-from unittest.mock import patch, MagicMock
-
 import pytest
-from django.core.exceptions import ValidationError
+from unittest.mock import patch
 
-from polaris.tests.sep38 import BaseSep38Tests
+from stellar_sdk import Keypair
+
+from polaris.sep38.utils import asset_id_format
+from polaris.tests.helpers import mock_check_auth_success
+from polaris.models import DeliveryMethod, Asset, OffChainAsset, ExchangePair
 
 
-class TestQuote(BaseSep38Tests):
-    @classmethod
-    def setUpClass(cls):
-        cls.mock_get_significant_decimals = mock.patch(
-            "polaris.sep38.utils.get_significant_decimals"
-        ).start()
-        BaseSep38Tests.setUpClass()
+ENDPOINT = "/sep38/quote"
+code_path = "polaris.sep38.quote"
 
-    @classmethod
-    def tearDownClass(cls):
-        BaseSep38Tests.tearDownClass()
 
-    def tearDown(self) -> None:
-        super().tearDown()
-        self.mock_get_significant_decimals.reset_mock()
+def default_data():
+    usd_stellar = Asset.objects.create(
+        code="usd", issuer=Keypair.random().public_key, sep38_enabled=True
+    )
+    brl_offchain = OffChainAsset.objects.create(
+        scheme="iso4217", identifier="BRL", country_codes="BRA"
+    )
+    brl_offchain.delivery_methods.add(
+        *[
+            DeliveryMethod.objects.create(
+                type=DeliveryMethod.TYPE.buy,
+                name="cash_pickup",
+                description="cash pick-up",
+            ),
+            DeliveryMethod.objects.create(
+                type=DeliveryMethod.TYPE.sell,
+                name="cash_dropoff",
+                description="cash drop-off",
+            ),
+        ]
+    )
+    pair = ExchangePair.objects.create(
+        buy_asset=asset_id_format(brl_offchain), sell_asset=asset_id_format(usd_stellar)
+    )
+    return {
+        "stellar_assets": [usd_stellar],
+        "offchain_assets": [brl_offchain],
+        "exchange_pairs": [pair],
+    }
 
-    def test_validate_quote_request(self):
-        test_data = {
-            "sell_asset": "stellar:SRT:GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B",
-            "buy_asset": "iso4217:USD",
-            "sell_amount": "10",
-            "buy_amount": None,
-            "expire_after": "2021-10-01 10:00:00",
-        }
-        # Happy path
-        from polaris.sep38.quote import validate_quote_request
 
-        validate_quote_request(test_data)
-
-        test_data.update(
-            {
-                "sell_asset": "stellar:SRT:GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B",
-                "buy_asset": "iso4217:USD",
-                "sell_amount": "10",
-                "buy_amount": "10",
-            }
-        )
-        with pytest.raises(ValidationError):
-            validate_quote_request(test_data)
-
-        test_data.update(
-            {
-                "sell_asset": "stellar:SRT:GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B",
-                "buy_asset": "iso4217:USD",
-                "sell_amount": None,
-                "buy_amount": None,
-            }
-        )
-        with pytest.raises(ValidationError):
-            validate_quote_request(test_data)
-
-        test_data.update(
-            {
-                "sell_asset": None,
-                "buy_asset": "iso4217:USD",
-                "sell_amount": "10",
-                "buy_amount": None,
-            }
-        )
-        with pytest.raises(ValidationError):
-            validate_quote_request(test_data)
-
-        test_data.update(
-            {
-                "sell_asset": "stellar:SRT:GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B",
-                "buy_asset": None,
-                "sell_amount": "10",
-                "buy_amount": None,
-            }
-        )
-        with pytest.raises(ValidationError):
-            validate_quote_request(test_data)
-
-        # Test invalid date string
-        test_data.update(
-            {
-                "sell_asset": "stellar:SRT:GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B",
-                "buy_asset": "iso4217:USD",
-                "sell_amount": None,
-                "buy_amount": "10",
-                "expire_after": "abc",
-            }
-        )
-        with pytest.raises(ValueError):
-            validate_quote_request(test_data)
-
-    def test_get_quote(self):
-        from polaris.models import Quote
-
-        mock_request = MagicMock()
-        test_quote = Quote()
-        # Happy path
-        self.mock_get_quote_by_id.return_value = test_quote
-        from polaris.sep38.quote import QuoteAPIView
-
-        QuoteAPIView.get(mock_request, "1234")
-        self.mock_get_quote_by_id.assert_called_with("1234")
-        assert self.mock_error.call_count == 0
-
-        self.mock_get_quote_by_id.side_effect = ValueError("")
-        QuoteAPIView.get(mock_request, "1234")
-        assert self.mock_error.call_count == 1
-
-    @patch("polaris.integrations.registered_quote_integration.post_quote")
-    @patch("polaris.sep38.quote.validate_quote_request")
-    def test_post_quote_error(
-        self, mock_validate_quote_request: MagicMock, mock_rqi_post_quote: MagicMock
-    ):
-        mock_validate_quote_request.side_effect = ValidationError("")
-        mock_request = MagicMock()
-
-        from polaris.sep38.quote import QuoteAPIView
-
-        QuoteAPIView.post(mock_request)
-        self.mock_error.assert_called_once()
-
-        self.mock_error.reset_mock()
-        mock_validate_quote_request.side_effect = None
-        self.mock_get_exchange_pair.side_effect = ValueError("")
-        QuoteAPIView.post(mock_request)
-        self.mock_error.assert_called_once()
-
-        # Test None exchange pair should result in error
-        self.mock_error.reset_mock()
-        self.mock_get_exchange_pair.side_effect = None
-        self.mock_get_exchange_pair.return_value = None
-        QuoteAPIView.post(mock_request)
-        self.mock_error.assert_called_once()
-
-        # Test None rqi.post_quote should result in error
-        from polaris.models import ExchangePair
-
-        self.mock_error.reset_mock()
-        self.mock_get_exchange_pair.side_effect = None
-        ep = ExchangePair()
-        ep.__dict__.update(
-            {
-                "sell_asset": "stellar:SRT:GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B",
-                "buy_asset": "iso4217:NGN",
-            }
-        )
-        self.mock_get_exchange_pair.return_value = ep
-        mock_rqi_post_quote.return_value = None
-        QuoteAPIView.post(mock_request)
-        self.mock_error.assert_called_once()
-
-    @patch("polaris.integrations.registered_quote_integration.post_quote")
-    @patch("polaris.sep38.quote.validate_quote_request")
-    @patch("polaris.models.Quote.save")
-    def test_post_quote_ok(
-        self,
-        mock_quote_save: MagicMock,
-        mock_validate_quote_request: MagicMock,
-        mock_rqi_post_quote: MagicMock,
-    ):
-        mock_request = MagicMock()
-        test_request = {
-            "sell_asset": "iso4217:NGN",
-            "buy_asset": "stellar:SRT:GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B",
-            "sell_amount": 10,
-            "expire_after": "2021-10-01T10:00:00",
-            "sell_delivery_method": "cash",
-            "buy_delivery_method": "cash",
-            "country_code": "USA",
-        }
-
-        test_quote_result = {
-            "sell_asset": "iso4217:NGN",
-            "buy_asset": "stellar:SRT:GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B",
-            "sell_amount": "10",
-            "buy_amount": None,
-            "requested_expire_after": "2021-10-01T10:00:00",
-            "sell_delivery_method": "cash",
-            "buy_delivery_method": "cash",
-            "country_code": "USA",
-            "price": "0.5",
-        }
-
-        # Test with sell_amount
-        from polaris.models import Quote
-
-        test_quote = Quote()
-        test_quote.__dict__.update(test_quote_result)
-
-        # Test with sell_amount
-        from polaris.models import Quote
-
-        test_quote = Quote()
-        test_quote.__dict__.update(test_quote_result)
-
-        from polaris.models import ExchangePair
-
-        ep = ExchangePair()
-        ep.__dict__.update(
-            {
-                "sell_asset": "stellar:SRT:GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B",
-                "buy_asset": "iso4217:NGN",
-            }
-        )
-        self.mock_get_exchange_pair.return_value = ep
-
-        mock_quote_save.reset_mock()
-        self.mock_get_exchange_pair.side_effect = None
-        self.mock_get_exchange_pair.return_value = ep
-        self.mock_get_significant_decimals.return_value = 2
-        mock_rqi_post_quote.return_value = test_quote
-        mock_validate_quote_request.return_value = test_request
-        from polaris.sep38.quote import QuoteAPIView
-
-        QuoteAPIView.post(mock_request)
-        mock_quote_save.assert_called_once()
-        saved_quote = mock_quote_save.call_args[0][0]
-        assert float(saved_quote.sell_amount) / float(saved_quote.buy_amount) == float(
-            saved_quote.price
-        )
-
-        # Test with buy amount
-        mock_quote_save.reset_mock()
-        test_quote.__dict__.update({"sell_amount": None, "buy_amount": "10"})
-        QuoteAPIView.post(mock_request)
-        mock_quote_save.assert_called_once()
-        saved_quote = mock_quote_save.call_args[0][0]
-        assert float(saved_quote.sell_amount) / float(saved_quote.buy_amount) == float(
-            saved_quote.price
-        )
+@pytest.mark.django_db
+@patch(f"{code_path}.rqi")
+@patch("polaris.sep10.utils.check_auth", mock_check_auth_success)
+def test_post_quote_success_no_optional_params(mock_rqi, client):
+    pass
