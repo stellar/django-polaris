0751542c84b18982a4a670383b4c563dbfee0042
Jake Urban <jake@stellar.org>
2021-11-17 11:19:18 -0500

    bump polaris to 2.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -33,9 +33,9 @@ copyright = "2019, Jake Urban"
 author = "Jake Urban"
 
 # The short X.Y version
-version = "2.1.0"
+version = "2.1.1"
 # The full version, including alpha/beta/rc tags
-release = "2.1.0"
+release = "2.1.1"
 
 
 # -- General configuration ---------------------------------------------------
