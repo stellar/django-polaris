37115b516f1254298306dc23f30d3c0cf7959761
Jake Urban <jake@stellar.org>
2021-11-29 13:36:24 -0800

    update script name, remove console logs

--- polaris/polaris/static/polaris/scripts/timezone.js
+++ polaris/polaris/static/polaris/scripts/timezone.js
@@ -4,8 +4,6 @@ function fetchTimezone({tzEndpoint, sessionId, sessionOffset }) {
   if (!(tzEndpoint && sessionId))
     return;
   const currentOffset = new Date().getTimezoneOffset() * -1;
-  console.log(currentOffset)
-  console.log(sessionOffset)
   if (currentOffset == sessionOffset)
     return;
   fetch(tzEndpoint, {
