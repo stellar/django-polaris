a185a9d2b0fdbf45dada73627d0b5b7279d78f56
Jake Urban <jake@stellar.org>
2023-03-01 09:42:51 -0700

    use f-style string formatting

--- polaris/integrations/fees.py
+++ polaris/integrations/fees.py
@@ -51,10 +51,7 @@ def calculate_fee(
         fee_percent = asset.send_fee_percent
         fee_fixed = asset.send_fee_fixed
     else:
-        raise ValueError(
-            ("Unexpected operation: %(operation)s")
-            % {"operation": fee_params["operation"]}
-        )
+        raise ValueError(f"unexpected operation: {fee_params['operation']}")
 
     if fee_fixed is None and fee_percent is None:
         return Decimal(0)
